95401cb733d7770c6e310f455a1a476263b36e2c
RoseBud13 <xiongkaijie13@gmail.com>
2025-04-29 00:42:24 +0800

    feat: build basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-container">
+        <div className="app-header">
+          <h1># </h1>
+          <h1 style={{ color: '#03a7dd' }}>MDC</h1>
+          <h2>onverter</h2>
+          <pre> markdown to everything</pre>
+        </div>
+        <div className="app-content">
+          <div className="markdown-area"></div>
+          <div className="preview-area"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
